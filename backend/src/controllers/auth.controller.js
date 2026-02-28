f8c679cf02fd6cb33eebf9eb6ece3fce9805b986
Poly1999 <pikalova.developer@gmail.com>
2026-02-28 13:23:26 -0500

    login & logout refactoring

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -66,6 +66,11 @@ export const signup = async (req, res) => {
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: 'Email and password are required' });
+  }
+
   try {
     const user = await User.findOne({ email });
     if (!user) return res.status(400).json({ message: 'Invalid credentials' });
@@ -90,6 +95,11 @@ export const login = async (req, res) => {
 };
 
 export const logout = (req, res) => {
-  res.cookie('jwt', '', { maxAge: 0 });
+  res.cookie('jwt', '', {
+    maxAge: 0,
+    secure: ENV.NODE_ENV !== 'development',
+    httpOnly: true,
+    sameSite: 'strict',
+  });
   res.status(200).json({ message: 'Logged out successfully' });
 };
