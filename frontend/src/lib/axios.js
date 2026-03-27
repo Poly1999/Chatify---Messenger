4ee26b643da57fe4bce31cdf798c0b1e9fafef8c
Poly1999 <pikalova.developer@gmail.com>
2026-03-27 14:54:11 -0400

    finish

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,13 +1,3 @@
-// import axios from 'axios';
-
-// export const axiosInstance = axios.create({
-//   baseURL:
-//     import.meta.env.MODE === 'development'
-//       ? 'http://localhost:3000/api'
-//       : '/api',
-//   withCredentials: true,
-// });
-
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
