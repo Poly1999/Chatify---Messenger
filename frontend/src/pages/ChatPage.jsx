d782d5403c6b0d0350712e679a8e388d387f7d8d
Poly1999 <pikalova.developer@gmail.com>
2026-03-28 12:41:44 -0400

    fix: correct image validation logic in sendMessage controller

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -8,28 +8,6 @@ import ContactsList from '../components/ContactsList';
 import ChatContainer from '../components/ChatContainer';
 import NoConversationPlaceholder from '../components/NoConversationPlaceholder';
 
-// function ChatPage() {
-//   const { activeTab, selectedUser } = useChatStore();
-//   console.log('selectedUser:', selectedUser);
-//   return (
-//     <div className='relative w-full max-w-6xl h-[800px]'>
-//       <BorderAnimatedContainer className='flex flex-col md:flex-row h-full'>
-//         {/* LEFT SIDE */}
-//         <div className='w-full md:w-80 bg-slate-800/50 backdrop-blur-sm flex flex-col'>
-//           <ProfileHeader />
-//           <ActiveTabSwitch />
-
-//           <div className='flex-1 overflow-y-auto p-4 space-y-2'>
-//             {activeTab === 'chats' ? <ChatList /> : <ContactsList />}
-//           </div>
-//         </div>
-
-//         {/* RIGHT SIDE */}
-//         <div className='flex-1 flex flex-col bg-slate-900/50 backdrop-blur-sm'>
-//           {selectedUser ? <ChatContainer /> : <NoConversationPlaceholder />}
-//         </div>
-//       </BorderAnimatedContainer>
-//     </div>
 function ChatPage() {
   const { activeTab, selectedUser } = useChatStore();
 
